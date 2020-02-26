1f6e894d5630982721f9453f4896558e82122384
JohannesHupfeld <j.hupfeld@gmail.com>
2020-02-26 17:25:42 -0500

    first commit

---
+++ lib/portland_best_bars.rb
@@ -0,0 +1,6 @@
+require "portland_best_bars/version"
+
+module PortlandBestBars
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/portland_best_bars/version.rb
@@ -0,0 +1,3 @@
+module PortlandBestBars
+  VERSION = "0.1.0"
+end
